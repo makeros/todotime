e3858e848bcf72920d11fef86615659793f3fd9e
makeros <mail@arekczogala.pl>
2020-03-16 09:44:47 +0100

    feat: save api key and refresh time interal in muinites from preferences page

---
+++ frontend/src/UI.elm
@@ -0,0 +1,28 @@
+module UI exposing (..)
+
+import Css exposing (..)
+import Css.Global exposing (body, global)
+import Html
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (class, value)
+import Html.Styled.Events exposing (onClick, onInput)
+
+
+theme : { secondary : Color, primary : Color }
+theme =
+    { primary = hex "010101"
+    , secondary = hex "f1f1f1"
+    }
+
+
+btnSave : List (Attribute msg) -> List (Html msg) -> Html msg
+btnSave =
+    button [ class "uk-button uk-button-primary" ]
+
+
+inputField : List (Attribute msg) -> List (Html msg) -> Html msg
+inputField =
+    styled input
+        [ padding (px 8)
+        , width (pct 100)
+        ]
