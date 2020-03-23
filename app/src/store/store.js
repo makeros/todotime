8d850936b72adb8b73f2b63bc7414ff943883ed8
makeros <mail@arekczogala.pl>
2020-03-23 18:15:35 +0100

    feat: #13 show last successful sync time in the tray context menu

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -46,11 +46,4 @@ function parseDataFile (filePath, defaults) {
   }
 }
 
-module.exports = new Store({
-  configName: 'user-settings',
-  defaults: {
-    apiKey: '',
-    refreshTimeInterval: 1000 * 60 * 60,
-    todoistLabel: "t-<minutes>"
-  }
-})
+module.exports = Store
